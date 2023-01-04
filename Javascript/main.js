20a7b6e7bd41d2240a8380f5a017f872d5e1346c
Jay <chanathip2320@gmail.com>
2023-01-04 11:43:06 +0700

    simple submit form

---
+++ Javascript/main.js
@@ -0,0 +1,22 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+
+    formValidation();
+});
+
+
+let formValidation = () => {
+    if (input.value === "") { //prevent use enter an emty form
+        msg.innerHTML = "Post cannot be blank";
+        console.log("failure");
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+    }
+};
