d73119478fb7b384ec4f35368d1898422e4b662d
Jay <chanathip2320@gmail.com>
2023-01-04 12:33:27 +0700

    delete post

--- Javascript/main.js
+++ Javascript/main.js
@@ -14,9 +14,36 @@ form.addEventListener("submit", (e) => {
 let formValidation = () => {
     if (input.value === "") { //prevent use enter an emty form
         msg.innerHTML = "Post cannot be blank";
-        console.log("failure");
+        console.error("failure");
     } else {
         console.log("success");
         msg.innerHTML = "";
+        acceptData(); //used when user press the submit button 
     }
 };
+
+let data = {};
+
+let acceptData = () => { //Fired when browser throw the error
+    data["text"] = input.value;
+    console.log(data);
+
+    createPost();
+};
+
+let createPost = () => { //create a new template
+    posts.innerHTML += `
+    <div>
+    <p>${data.text}</p>
+    <span class = "options">
+        <i onClick="editPost(this)" class ="fas fa-edit" ></i>
+        <i onclick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+    </div>
+    `;
+    input.value = "";
+}
+
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove(); //parentElement used to specify the parent of that class
+}
